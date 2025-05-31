1a34735578d69d68cb22ff876c1e64c7a43027fe
carrantal <carrantal1@gmail.com>
2025-05-31 15:08:51 +0500

    Update OffSale2.jsx

--- src/app/components/OffSale/OffSale.jsx
+++ src/app/components/OffSale/OffSale.jsx
@@ -45,7 +45,7 @@ const OffSale = ({ carOffers }) => {
               <div className="d-flex justify-content-between align-items-center">
                 <div className="d-flex flex-column">
                   <span className="text-[#ec7421] hourly-offers-container-title">
-                    Special Ramadan Offer
+                    Special Summers Offer
                   </span>
                   <span className="text-[#ec7421] hourly-offers-container-sub-title">
                     Rent hourly and save with limited time offers
